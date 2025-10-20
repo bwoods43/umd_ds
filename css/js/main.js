dc04c754bb36a7bc5c32e5ff6ecdd1f2b0139cdd
Ben Woods <bwoods16@umd.edu>
2025-10-20 16:57:09 -0400

    #18 first commit of theme files

---
+++ css/js/main.js
@@ -0,0 +1,48 @@
+(function ($) {
+   "use strict"; 
+  $(window).on('load', function(){
+   
+		/* https://www.w3schools.com/howto/howto_js_accordion.asp */ 
+  	var acc = document.getElementsByClassName("accordion-headline");
+		var i;
+
+		for (i = 0; i < acc.length; i++) {
+  		acc[i].addEventListener("click", function() {
+    		/* Toggle between adding and removing the "active" class,
+    		to highlight the button that controls the panel */
+    		this.classList.toggle("active");
+
+    		/* Toggle between hiding and showing the active panel */
+    		var panel = this.nextElementSibling;
+    		console.log(this.nextElementSibling);
+    		console.log(panel.style.display);
+    		if (panel.style.display === "block") {
+      		panel.style.display = "none";
+      		panel.style.height = "0";
+    		} else {
+      		panel.style.display = "block";
+      		panel.style.height = "auto";
+    		}
+  		});
+		}
+  
+		/* basic play button for name pronunciation */
+		// Get references to the HTML elements
+		const playButton = document.getElementById('playButton');
+		const playLink = document.getElementById('playLink');
+		const myAudio = document.getElementById('myAudio');
+
+		// Add an event listener to the button
+		playButton.addEventListener('click', () => {
+    	// Play the audio when the button is clicked
+    	myAudio.play();
+		});  
+
+		// Add an event listener to the link
+		playLink.addEventListener('click', () => {
+    	// Play the audio when the button is clicked
+    	myAudio.play();
+		});  
+
+  });
+})(jQuery);
